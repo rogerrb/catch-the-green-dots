0f7d979dd657eede6e8fed385c10f65d80618de3
rogerrb <roger_R_berg@hotmail.com>
2020-11-30 18:04:19 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,95 @@
+sprites.onDestroyed(SpriteKind.Food, function (sprite) {
+    info.changeScoreBy(1)
+    Ekstraliv += 1
+    if (Ekstraliv > 9) {
+        info.changeLifeBy(1)
+        Ekstraliv = 0
+    } else if (info.score() >= 20) {
+        game.over(true)
+    }
+})
+controller.anyButton.onEvent(ControllerButtonEvent.Released, function () {
+    mySprite.setVelocity(0, 0)
+})
+sprites.onOverlap(SpriteKind.Food, SpriteKind.Player, function (sprite, otherSprite) {
+    sprite.destroy()
+})
+sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Player, function (sprite, otherSprite) {
+    sprite.destroy()
+})
+info.onLifeZero(function () {
+    game.over(false, effects.confetti)
+})
+sprites.onDestroyed(SpriteKind.Enemy, function (sprite) {
+    info.changeLifeBy(-1)
+})
+let mySprite2: Sprite = null
+let mySprite: Sprite = null
+let Ekstraliv = 0
+Ekstraliv = 0
+info.setScore(0)
+info.setLife(3)
+mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . 3 3 3 3 3 3 3 . . . . 
+    . . . . 3 3 3 3 3 3 3 3 3 . . . 
+    . . . 3 3 3 3 3 3 3 3 3 3 3 . . 
+    . . 3 3 3 3 3 3 3 3 3 3 3 3 3 . 
+    . . 3 3 3 3 3 3 3 3 3 3 3 3 3 . 
+    . . 3 3 3 3 3 3 3 3 3 3 3 3 3 . 
+    . . 3 3 3 3 3 3 3 3 3 3 3 3 3 . 
+    . . 3 3 3 3 3 3 3 3 3 3 3 3 3 . 
+    . . 3 3 3 3 3 3 3 3 3 3 3 3 3 . 
+    . . 3 3 3 3 3 3 3 3 3 3 3 3 3 . 
+    . . . 3 3 3 3 3 3 3 3 3 3 3 . . 
+    . . . . 3 3 3 3 3 3 3 3 3 . . . 
+    . . . . . 3 3 3 3 3 3 3 . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+mySprite.setPosition(73, 56)
+controller.moveSprite(mySprite)
+game.onUpdateInterval(1000, function () {
+    mySprite2 = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . 8 8 8 8 8 8 8 8 8 8 8 8 8 . 
+        . . 8 8 8 8 8 8 8 8 8 8 8 8 8 . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Enemy)
+    mySprite2.setPosition(randint(0, 160), 0)
+    mySprite2.setVelocity(0, 10)
+})
+game.onUpdateInterval(1000, function () {
+    mySprite2 = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . 7 7 7 7 7 7 7 . . . . 
+        . . . . . 7 7 7 7 7 7 7 . . . . 
+        . . . . . 7 7 7 7 7 7 7 . . . . 
+        . . . . . 7 7 7 7 7 7 7 . . . . 
+        . . . . . 7 7 7 7 7 7 7 . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Food)
+    mySprite2.setPosition(randint(0, 160), 0)
+    mySprite2.setVelocity(0, 10)
+})
