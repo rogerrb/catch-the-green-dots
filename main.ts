100637403b4d7eb5d9080398e17e4a2061e937c1
rogerrb <roger_R_berg@hotmail.com>
2020-12-07 17:39:54 +0100

    Shooting fixed and sounds added

--- main.ts
+++ main.ts
@@ -1,58 +1,69 @@
 sprites.onDestroyed(SpriteKind.Food, function (sprite) {
     info.changeScoreBy(1)
+    music.powerUp.play()
     Ekstraliv += 1
     if (Ekstraliv > 9) {
+        music.baDing.play()
         info.changeLifeBy(1)
         Ekstraliv = 0
-    } else if (info.score() >= 20) {
+    } else if (info.score() >= 999) {
         game.over(true)
     }
 })
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    mySprite3 = sprites.create(img`
+    projectile = sprites.createProjectileFromSprite(img`
         . . . . . . . . . . . . . . . . 
+        . . . . . . 4 4 4 4 . . . . . . 
+        . . . . 4 4 4 5 5 4 4 4 . . . . 
+        . . . 3 3 3 3 4 4 4 4 4 4 . . . 
+        . . 4 3 3 3 3 2 2 2 1 1 4 4 . . 
+        . . 3 3 3 3 3 2 2 2 1 1 5 4 . . 
+        . 4 3 3 3 3 2 2 2 2 2 5 5 4 4 . 
+        . 4 3 3 3 2 2 2 4 4 4 4 5 4 4 . 
+        . 4 4 3 3 2 2 4 4 4 4 4 4 4 4 . 
+        . 4 2 3 3 2 2 4 4 4 4 4 4 4 4 . 
+        . . 4 2 3 3 2 4 4 4 4 4 2 4 . . 
+        . . 4 2 2 3 2 2 4 4 4 2 4 4 . . 
+        . . . 4 2 2 2 2 2 2 2 2 4 . . . 
+        . . . . 4 4 2 2 2 2 4 4 . . . . 
+        . . . . . . 4 4 4 4 . . . . . . 
         . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . 2 . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        `, SpriteKind.Projectile)
-    mySprite3.setPosition(mySprite.x, mySprite.y)
-    mySprite3.setVelocity(mySprite.vx, mySprite.vy)
+        `, Player1, 0, -50)
+    music.pewPew.play()
 })
 controller.anyButton.onEvent(ControllerButtonEvent.Released, function () {
-    mySprite.setVelocity(0, 0)
+    Player1.setVelocity(0, 0)
 })
 sprites.onOverlap(SpriteKind.Food, SpriteKind.Player, function (sprite, otherSprite) {
     sprite.destroy()
 })
 sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Player, function (sprite, otherSprite) {
+    music.jumpDown.play()
+    info.changeLifeBy(-1)
     sprite.destroy()
 })
 info.onLifeZero(function () {
     game.over(false, effects.splatter)
 })
 sprites.onDestroyed(SpriteKind.Enemy, function (sprite) {
+    info.changeScoreBy(1)
+    music.magicWand.play()
+})
+sprites.onDestroyed(SpriteKind.Player, function (sprite) {
     info.changeLifeBy(-1)
 })
-let mySprite2: Sprite = null
-let mySprite3: Sprite = null
-let mySprite: Sprite = null
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+    otherSprite.destroy()
+})
+let Enemy1: Sprite = null
+let Food1: Sprite = null
+let projectile: Sprite = null
+let Player1: Sprite = null
 let Ekstraliv = 0
 Ekstraliv = 0
 info.setScore(0)
 info.setLife(3)
-mySprite = sprites.create(img`
+Player1 = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . 3 3 3 3 3 3 3 . . . . 
     . . . . 3 3 3 3 3 3 3 3 3 . . . 
@@ -70,32 +81,10 @@ mySprite = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
-mySprite.setPosition(73, 56)
-controller.moveSprite(mySprite)
-game.onUpdateInterval(1000, function () {
-    mySprite2 = sprites.create(img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . 8 8 8 8 8 8 8 8 8 8 8 8 8 . 
-        . . 8 8 8 8 8 8 8 8 8 8 8 8 8 . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        `, SpriteKind.Enemy)
-    mySprite2.setPosition(randint(0, 160), 0)
-    mySprite2.setVelocity(0, 10)
-})
-game.onUpdateInterval(1000, function () {
-    mySprite2 = sprites.create(img`
+Player1.setPosition(73, 56)
+controller.moveSprite(Player1)
+game.onUpdateInterval(5000, function () {
+    Food1 = sprites.create(img`
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
@@ -113,6 +102,28 @@ game.onUpdateInterval(1000, function () {
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
         `, SpriteKind.Food)
-    mySprite2.setPosition(randint(0, 160), 0)
-    mySprite2.setVelocity(0, 10)
+    Food1.setPosition(randint(0, 160), 0)
+    Food1.setVelocity(0, 10)
+})
+game.onUpdateInterval(1000, function () {
+    Enemy1 = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . 8 8 8 8 8 8 8 8 8 8 8 8 8 . 
+        . . 8 8 8 8 8 8 8 8 8 8 8 8 8 . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Enemy)
+    Enemy1.setPosition(randint(0, 160), 0)
+    Enemy1.setVelocity(0, 10)
 })
